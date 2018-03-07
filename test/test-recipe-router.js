4d4a17b99eed3ec9de5a27a2194c4e60abe138cd
Jorge R <jorgerodriguez434@gmail.com>
2018-03-07 09:17:13 -0600

    pass test for GET and POST

--- test/test-recipe-router.js
+++ test/test-recipe-router.js
@@ -1,25 +1,78 @@
 const chai = require('chai');
 const chaiHttp = require('chai-http');
 const expect = chai.expect;
-const {app} = require('../server');
+const {app, runServer, closeServer} = require('../server');
 
 chai.use(chaiHttp);
 
 describe('Recipes', function() {
 
+      before(function() {
+            return runServer();
+      });
+
+
+      after(function() {
+          return closeServer();
+      });
+
       it('should list recipes on GET', function() {
             return chai.request(app)
-            .get('recipes')
+            .get('/recipes')
             .then(function(res) {
 
                 expect(res).to.have.status(200);
-                expect(res).to.have.json();
+                expect(res).to.be.json;
   
+            });
+
+
+      });
+
+      it('should add recipes on POST', function() {
+            const newRecipe = { name: 'foo', ingredients: ['buzz', 'fizz']};            
+            return chai.request(app)
+            .post('/recipes')
+            .send(newRecipe)
+            .then(function(res) {
+
+                expect(res).to.have.status(201);
+                expect(res).to.be.json;
+  
+            });
+            
+
+
+      }); 
+
+       it('should update recipes on PUT', function() {
+            const updateItem = {
+                        name: 'foo',
+                        ingredients: ['bizz', 'bang']
+              };
+
+            return chai.request(app)
+            .get('/recipes')
+            .then(function(res) {
+
+                  updateItem.id = res.body[0].id;
+                  return chai.request(app)
+                  .put(`/recipes/${updateItem.id}`)
+                  .send(updateItem);
+
+            })
+            .then(function(res) {
+
+                expect(res).to.have.status(200);
+                expect(res).to.be.json;
+                expect(res.body).to.deep.equal(updateItem);
+
 
             });
 
 
       }); 
+
 
 
 });
